bfb345851cca260569251220312bf857fe6e8cb5
Askar Imran <askar.kani@xero.com>
2021-08-29 20:08:51 +1000

    Solution reducer and callback

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -11,19 +11,16 @@ import {
 } from '../pokemon'
 
 // 🐨 this is going to be our generic asyncReducer
-function pokemonInfoReducer(state, action) {
+function asyncReducer(state, action) {
   switch (action.type) {
     case 'pending': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'pending', pokemon: null, error: null}
+      return {status: 'pending', data: null, error: null}
     }
     case 'resolved': {
-      // 🐨 replace "pokemon" with "data" (in the action too!)
-      return {status: 'resolved', pokemon: action.pokemon, error: null}
+      return {status: 'resolved', data: action.data, error: null}
     }
     case 'rejected': {
-      // 🐨 replace "pokemon" with "data"
-      return {status: 'rejected', pokemon: null, error: action.error}
+      return {status: 'rejected', data: null, error: action.error}
     }
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
@@ -31,53 +28,55 @@ function pokemonInfoReducer(state, action) {
   }
 }
 
-function PokemonInfo({pokemonName}) {
-  // 🐨 move both the useReducer and useEffect hooks to a custom hook called useAsync
-  // here's how you use it:
-  // const state = useAsync(
-  //   () => {
-  //     if (!pokemonName) {
-  //       return
-  //     }
-  //     return fetchPokemon(pokemonName)
-  //   },
-  //   {status: pokemonName ? 'pending' : 'idle'},
-  //   [pokemonName],
-  // )
-  // 🐨 so your job is to create a useAsync function that makes this work.
-  const [state, dispatch] = React.useReducer(pokemonInfoReducer, {
-    status: pokemonName ? 'pending' : 'idle',
-    // 🐨 this will need to be "data" instead of "pokemon"
-    pokemon: null,
-    error: null,
-  })
+function useSafeDispatch(dispatch){
+  const isMounted = React.useRef(false);
+  
+  React.useEffect(()=>{
+    isMounted.current = true;
+    return () => isMounted.current = false;
+  }, []);
+  
+  return React.useCallback((...args) => {
+    if(isMounted.current) {
+      dispatch(...args)
+    }
+  },[dispatch])
+}
 
+function useAsync({status}) { 
+
+  const [state, unSafeDispatch] = React.useReducer(asyncReducer, {
+    status,
+    data: null,
+    error: null,
+  });
+
+  const dispatch = useSafeDispatch(unSafeDispatch);
+
+
+  const run = React.useCallback((promise) => {
+    if (!promise) {
+      return
+    }    
+    dispatch({type: 'pending'})
+    promise.then((data) => {
+        dispatch({type: 'resolved', data})      
+    }).catch(error => {
+      console.log('catch block');
+      dispatch({type: 'rejected', error})
+    })
+  },[dispatch])
+  return {...state, run};
+}
+
+function PokemonInfo({pokemonName}) {
+  const {status, error, run, data} = useAsync({status: pokemonName ? 'pending' : 'idle'});
   React.useEffect(() => {
-    // 💰 this first early-exit bit is a little tricky, so let me give you a hint:
-    // const promise = asyncCallback()
-    // if (!promise) {
-    //   return
-    // }
-    // then you can dispatch and handle the promise etc...
-    if (!pokemonName) {
+    if(!pokemonName){
       return
     }
-    dispatch({type: 'pending'})
-    fetchPokemon(pokemonName).then(
-      pokemon => {
-        dispatch({type: 'resolved', pokemon})
-      },
-      error => {
-        dispatch({type: 'rejected', error})
-      },
-    )
-    // 🐨 you'll accept dependencies as an array and pass that here.
-    // 🐨 because of limitations with ESLint, you'll need to ignore
-    // the react-hooks/exhaustive-deps rule. We'll fix this in an extra credit.
-  }, [pokemonName])
-
-  // 🐨 this will change from "pokemon" to "data"
-  const {pokemon, status, error} = state
+    run(fetchPokemon(pokemonName))
+  },[pokemonName, run])
 
   if (status === 'idle' || !pokemonName) {
     return 'Submit a pokemon'
@@ -86,7 +85,7 @@ function PokemonInfo({pokemonName}) {
   } else if (status === 'rejected') {
     throw error
   } else if (status === 'resolved') {
-    return <PokemonDataView pokemon={pokemon} />
+    return <PokemonDataView pokemon={data} />
   }
 
   throw new Error('This should be impossible')
